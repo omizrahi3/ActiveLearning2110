8646e6e6fb77b5d6e4763ab266ecd283026f8709
kmaclin <maclin.kelsey@gmail.com>
2017-02-08 12:16:12 -0500

    update to course view page

--- app_client/app-components/sidebar/sidebar.controller.js
+++ app_client/app-components/sidebar/sidebar.controller.js
@@ -17,7 +17,6 @@ var app = angular.module('app');
 
 app.controller('Sidebar.Controller', function($scope, $element, $localStorage) {
 
-        $scope.photo = $localStorage.photo;
 
         $scope.courseClick = function(index) {
             $scope.$storage.selectedCourse = index;
